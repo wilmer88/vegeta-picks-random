1991374ca0634293e9467c9493eeeed0690b041c
wilmerbaby <wilmerbaby@gmail.com>
2020-09-11 13:55:11 -0400

    after working with tutor 9-7-20 and TA on 9-10-20 was able to create the amount of characters promt function and number genarator

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # vegeta-picks-random
+
+name of project: vageta-picks-random
+
+Date started: 9-1-20
+
+source: Georgia Tech Coding Bootcamp
+
+Was asked to code the javascript on the index.html given by the the bootcamp instructor.
+When you open index.html in browser, you can see written password generator and inside the the box you'll see a smaller box with secure password and a generate Password button.
+
+My job was to create prompts that asks the user, what criteria(numbers,letters and special charters) the user wants in the password that is going to be generated
--- assets/script.js
+++ assets/script.js
@@ -7,8 +7,59 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function catchedChar() {
+  var amountOfChar = prompt("how many characters in password? 8-120");
+if (amountOfChar < "8" && amountOfChar > "120");{
+  alert("must be between 8-120 characters")
+  amountOfChar();
+} else {
+  alert("go head")
+}
+
+confirmedNumbers = confirm("would you like to include numbers");
+
+var confirmedUpperCase = confirm("do you want to use upper case letters");
+
+ var confirmedLowercase = confirm("do you want to use lowercase letters"); //dummy prompt
+
+ var specialCharecter = confirm("do you want to use special characters"); //dummy prompt
+ 
+ var characterHolder = []; //holds charcters created by
+
+//number generator
+ if (confirmedNumbers == true) {
+   var numToGen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+   var randomPick = Math.floor(Math.random() * numToGen.length);
+
+  var ranNum = numToGen[randomPick];
+   characterHolder.push(ranNum);
+ }
+
+ for (i = 0; i < characterHolder; i++) {
+   var createdrandom = Math.floor(Math.random() * characterHolder);
+  var password = createdrandom[i]
+  }
+  password.push.generatePassword()
+
+
+// was able to recreate random specialcharacter/upper and lowercase same as the number Genarater
+// if (confirmedUpperCase == confirm) {
+//      var uperCaseLetters = ["A", "B", "C", "D", "E", "f" ];
+//      var ranUpperCase = Math.floor(Math.random() * numToGen);
+//      ranUpperCase.push("charecterHolder");
+//     }
+
+//     if ( confirmedLowercase == confirm) {
+//       var lowerCaseLetters = ["a", "b", "c", "d", "e", "f" ];
+//       var ranUpperCase = Math.floor(Math.random() * numToGen);
+//       ranUpperCase.push("charecterHolder");
+//      }
+ 
+// if ( specialCharecters == confirm) {
+//       var lowerCaseLetters = ["*", "%", "@", "!", "^", "-" ];
+//       var ranUpperCase = Math.floor(Math.random() * numToGen);
+//       var madeRanUpper = numToGen[ranUpperCase]
+//      }
