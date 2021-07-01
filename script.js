3b9e042802ab50063ab70cc679fc59451da24d2a
wilmerbaby <wilmerbaby@gmail.com>
2021-07-01 01:18:31 -0400

    added code that cleared old password on text area when generating new password

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// Assignment Code
+// declare variable
 var generateBtn = document.querySelector("#generate");
 var posiblePassword = [];
-var empty = [];
+var characterHolder = [];
+var specialCharacters = ["!", ">", "*", "#", "%", "~", "_", "^", ":", ")"];
 var letters = [
   "A",
   "B",
@@ -59,24 +60,30 @@ var num = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 var passwordText = document.querySelector("#password");
 
-// Write password to the #password input
+// main function
 function writePassword() {
+  passwordText.value = "";
+  posiblePassword = [];
+  characterHolder = [];
+
   var amountOfChar = parseInt(
     prompt("choose betweem 8-120 characters for password?")
   );
+  if (amountOfChar == false) {
+    return;
+  }
   if (amountOfChar < 8 || amountOfChar > 120 || isNaN(amountOfChar) === true) {
     alert(
       "password must be between 8-120 characters or amount of characters enterd must be a number "
     );
-    writePassword();
+    return;
   }
 
   var confirmedNumbers = confirm("Would you like to include numbers?");
   if (confirmedNumbers == true) {
     for (var i = 0; i < num.length; i++) {
-      empty = empty + num[i];
+      characterHolder = characterHolder + num[i];
     }
-    console.log(empty);
   }
 
   var confirmedUperCaseLetters = confirm(
@@ -84,38 +91,45 @@ function writePassword() {
   );
   if (confirmedUperCaseLetters == true) {
     for (var i = 0; i < letters.length; i++) {
-      empty = empty + letters[i];
+      characterHolder = characterHolder + letters[i];
     }
-    console.log(empty);
   }
   var confirmedLowercaseLetter = confirm(
     "would you like lower case letters in passoword"
   );
   if (confirmedLowercaseLetter == true) {
     for (var i = 0; i < lowerLetters.length; i++) {
-      empty = empty + lowerLetters[i];
+      characterHolder = characterHolder + lowerLetters[i];
     }
-    console.log(empty);
+  }
+  var confirmSpecialChar = confirm(
+    "would you like to include special characters in password?"
+  );
+  if (confirmSpecialChar == true) {
+    for (var i = 0; i < specialCharacters.length; i++) {
+      characterHolder = characterHolder + specialCharacters[i];
+    }
   }
   if (
     confirmedNumbers == true ||
     confirmedLowercaseLetter == true ||
-    confirmedUperCaseLetters == true
+    confirmedUperCaseLetters == true ||
+    confirmSpecialChar == true
   ) {
     for (var i = 0; i < amountOfChar; i++) {
-      randomNum = Math.floor(Math.random() * empty.length);
+      randomNum = Math.floor(Math.random() * characterHolder.length);
       console.log(randomNum);
-      posiblePassword = posiblePassword + empty[randomNum];
-      console.log(posiblePassword);
+      posiblePassword = posiblePassword + characterHolder[randomNum];
     }
     passwordText.value = posiblePassword;
   }
   if (
     confirmedNumbers == false &&
     confirmedLowercaseLetter == false &&
-    confirmedUperCaseLetters == false
+    confirmedUperCaseLetters == false &&
+    confirmSpecialChar == false
   ) {
-    alert("password must have numbers or letters");
+    alert("password must have numbers, letters, or special characters");
     writePassword();
   }
 }
@@ -124,40 +138,3 @@ function writePassword() {
 generateBtn.addEventListener("click", function () {
   writePassword();
 });
-
-// var confirmedUpperCase = confirm("do you want to use upper case letters");
-
-// var confirmedLowercase = confirm(
-//   "Would you like to include lowercase letters"
-// ); //dummy prompt
-
-// var specialCharecter = confirm("do you want to use special characters"); //dummy prompt
-
-// var characterHolder = []; //holds charcters created by
-
-// pushes randomcharacters from character holder to password variable
-//   for (i = 0; i < parseInt(amountOfChar); i++) {
-//     var createdrandom = Math.floor(Math.random() * characterHolder.length);
-//     var password = password.concat(createdrandom[i]);
-//   }
-//   password.push.generatePassword();
-//   alert(password + "Sorry only able to generate random number password");
-// });
-// recreated random specialcharacter/upper and lowercase same as the number Genarater
-// if (confirmedUpperCase == confirm) {
-//      var uperCaseLetters = ["A", "B", "C", "D", "E", "f" ];
-//      var ranUpperCase = Math.floor(Math.random() * numToGen);
-//      ranUpperCase.push("charecterHolder");
-//     }
-
-//     if ( confirmedLowercase == confirm) {
-//       var lowerCaseLetters = ["a", "b", "c", "d", "e", "f" ];
-//       var ranUpperCase = Math.floor(Math.random() * numToGen);
-//       ranUpperCase.push("charecterHolder");
-//      }
-
-// if ( specialCharecters == confirm) {
-//       var lowerCaseLetters = ["*", "%", "@", "!", "^", "-" ];
-//       var ranUpperCase = Math.floor(Math.random() * numToGen);
-//       var madeRanUpper = numToGen[ranUpperCase]
-// }
